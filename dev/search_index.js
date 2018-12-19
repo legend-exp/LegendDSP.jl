65c5a351109e5c8e89a9ba9066291a2e9bbf288a
autodocs <autodocs>
2018-12-19 14:00:27 +0000

    build based on 3d1b715

---
+++ dev/search_index.js
@@ -0,0 +1,75 @@
+var documenterSearchIndex = {"docs": [
+
+{
+    "location": "#",
+    "page": "Home",
+    "title": "Home",
+    "category": "page",
+    "text": ""
+},
+
+{
+    "location": "#LegendDSP.jl-1",
+    "page": "Home",
+    "title": "LegendDSP.jl",
+    "category": "section",
+    "text": ""
+},
+
+{
+    "location": "api/#",
+    "page": "API",
+    "title": "API",
+    "category": "page",
+    "text": ""
+},
+
+{
+    "location": "api/#API-1",
+    "page": "API",
+    "title": "API",
+    "category": "section",
+    "text": "DocTestSetup  = quote\n    using LegendDSP\nend"
+},
+
+{
+    "location": "api/#Types-1",
+    "page": "API",
+    "title": "Types",
+    "category": "section",
+    "text": "Order = [:type]"
+},
+
+{
+    "location": "api/#Functions-1",
+    "page": "API",
+    "title": "Functions",
+    "category": "section",
+    "text": "Order = [:function]"
+},
+
+{
+    "location": "api/#Documentation-1",
+    "page": "API",
+    "title": "Documentation",
+    "category": "section",
+    "text": "Modules = [LegendDSP]\nOrder = [:type, :function]"
+},
+
+{
+    "location": "LICENSE/#",
+    "page": "LICENSE",
+    "title": "LICENSE",
+    "category": "page",
+    "text": ""
+},
+
+{
+    "location": "LICENSE/#LICENSE-1",
+    "page": "LICENSE",
+    "title": "LICENSE",
+    "category": "section",
+    "text": "using Markdown\nMarkdown.parse_file(joinpath(@__DIR__, \"..\", \"..\", \"LICENSE.md\"))"
+},
+
+]}
