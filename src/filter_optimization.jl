c267f703911865175e791873a4b4082c186c2cb2
Florian Henkes <florian.henkes@outlook.com>
2023-07-19 15:38:37 +0200

    added filter optimization

--- src/filter_optimization.jl
+++ src/filter_optimization.jl
@@ -60,3 +60,63 @@ function dsp_trap_rt_optimization(wvfs::ArrayOfRDWaveforms, config::DSPConfig, �
     return enc_trap_grid
 end
 export dsp_trap_rt_optimization
+
+
+"""
+    dsp_trap_ft_optimization(wvfs::ArrayOfRDWaveforms, config::DSPConfig, τ::Quantity{T}, rt::Quantity{T}) where T<:Real
+
+Get energy grid values for given trap grid rise times while varying the flat-top time.
+
+Returns:
+    - `e_grid`: Array energy values for the given trap rise time grid at a given flat-top time grid.
+"""
+function dsp_trap_ft_optimization(wvfs::ArrayOfRDWaveforms, config::DSPConfig, τ::Quantity{T}, rt::Quantity{T}) where T<:Real
+    # get config parameters
+    bl_mean_min, bl_mean_max    = config.bl_mean
+    e_grid_ft_trap              = config.e_grid_ft_trap
+    t0_threshold                = config.t0_threshold
+    enc_pickoff_trap            = config.enc_pickoff_trap
+
+    # get baseline mean, std and slope
+    bl_stats = signalstats.(wvfs, bl_mean_min, bl_mean_max)
+
+    # substract baseline from waveforms
+    wvfs = shift_waveform.(wvfs, -bl_stats.mean)
+
+    # deconvolute waveform
+    deconv_flt = InvCRFilter(τ)
+    wvfs_pz = deconv_flt.(wvfs)
+
+    # t0 determination
+    # filter with fast asymetric trapezoidal filter and truncate waveform
+    uflt_asy_t0 = TrapezoidalChargeFilter(40u"ns", 100u"ns", 2000u"ns")
+    uflt_trunc_t0 = TruncateFilter(0u"µs"..60u"µs")
+
+    # eventuell zwei schritte!!!
+    wvfs_flt_asy_t0 = uflt_asy_t0.(uflt_trunc_t0.(wvfs_pz))
+
+    # get intersect at t0 threshold (fixed as in MJD analysis)
+    flt_intersec_t0 = Intersect(mintot = 600u"ns")
+
+    # get t0 for every waveform as pick-off at fixed threshold
+    t0 = uconvert.(u"µs", flt_intersec_t0.(wvfs_flt_asy_t0, t0_threshold).x)
+
+    # get energy grid for efficient optimization
+    e_grid   = Array{Union{Missing, Float32}}(missing, length(e_grid_ft_trap), length(wvfs_pz))
+    for (f, ft) in enumerate(e_grid_ft_trap)
+        if rt < ft
+            continue
+        end
+        uflt_rtft      = TrapezoidalChargeFilter(rt, ft)
+        
+        wvfs_flt_rtft  = uflt_rtft.(wvfs_pz)
+
+        e_rtft         = SignalEstimator(PolynomialDNI(4, 80u"ns")).(wvfs_flt_rtft, t0 .+ (rt + ft/2))
+
+        e_grid[f, :]     = e_rtft
+    end
+
+    return e_grid
+end
+export dsp_trap_ft_optimization
+
