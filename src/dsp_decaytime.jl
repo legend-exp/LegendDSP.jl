ef4b13aa40535713ae0c1246af5f8a2d5fabc9ea
sagitta42 <sssagitta@gmail.com>
2024-01-26 13:52:53 +0100

    dsp decay times mult dispatch

--- src/dsp_decaytime.jl
+++ src/dsp_decaytime.jl
@@ -7,11 +7,7 @@ Get statistics on the logarhithmic of the tail of the `wvfs` in the interval `ta
 # Returns
 - `τ`: decay time in µs
 """
-function dsp_decay_times(wvfs::ArrayOfRDWaveforms, config::DSPConfig)
-    # get config parameters
-    bl_window    = config.bl_window
-    tail_window  = config.tail_window
-
+function dsp_decay_times(wvfs::ArrayOfRDWaveforms, bl_window::ClosedInterval{<:Quantity{<:T}}, tail_window::ClosedInterval{<:Quantity{<:T}}) where T <: Real
     # get baseline mean, std and slope
     bl_stats = signalstats.(wvfs, first(bl_window), last(bl_window))
 
@@ -25,3 +21,5 @@ function dsp_decay_times(wvfs::ArrayOfRDWaveforms, config::DSPConfig)
     return uconvert.(u"µs", decay_times.τ)
 end
 export dsp_decay_times
+
+dsp_decay_times(wvfs::ArrayOfRDWaveforms, config::DSPConfig) = dsp_decay_times(wvfs, config.bl_window, config.tail_window)
