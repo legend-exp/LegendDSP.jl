87a3bbe52b9ffa6a35005903f748bcfe6be1723b
Florian Henkes <florian.henkes@outlook.com>
2024-02-06 12:33:03 +0100

    Changed window names to adapt to new layout

--- src/dsp_decaytime.jl
+++ src/dsp_decaytime.jl
@@ -1,31 +1,26 @@
 # This file is a part of LegendDSP.jl, licensed under the MIT License (MIT).
 
 """
-    dsp_decay_times(wvfs::AbstractSamples, start::Real, stop::Real)
-
-Get statistics on the logarhithmic of the tail of the `wvfs` in the interval (`start`,`stop`).
+    dsp_decay_times(wvfs::ArrayOfRDWaveforms, bl_window::ClosedInterval{<:Unitful.Time{<:T}}, tail_window::ClosedInterval{<:Unitful.Time{<:T}})
+    dsp_decay_times(wvfs::ArrayOfRDWaveforms, config::DSPConfig)
+    
+Get statistics on the logarhithmic of the tail of the `wvfs` in the interval `tail_window`.
 # Returns
 - `τ`: decay time in µs
 """
-function dsp_decay_times(wvfs::ArrayOfRDWaveforms, bl_mean::Tuple, pz_fit::Tuple)
-    # get config parameters
-    bl_mean_min, bl_mean_max    = bl_mean
-    pz_fit_min, pz_fit_max      = pz_fit
-
+function dsp_decay_times(wvfs::ArrayOfRDWaveforms, bl_window::ClosedInterval{<:Unitful.Time{<:T}}, tail_window::ClosedInterval{<:Unitful.Time{<:T}}) where T <: Real
     # get baseline mean, std and slope
-    bl_stats = signalstats.(wvfs, bl_mean_min, bl_mean_max)
+    bl_stats = signalstats.(wvfs, first(bl_window), last(bl_window))
 
     # substract baseline from waveforms
     wvfs_bl = shift_waveform.(wvfs, -bl_stats.mean)
 
     # extract decay times
-    decay_times = tailstats.(wvfs_bl, pz_fit_min, pz_fit_max)
+    decay_times = tailstats.(wvfs_bl, first(tail_window), last(tail_window))
     
     # return converted to µs vals
     return uconvert.(u"µs", decay_times.τ)
 end
-
-function dsp_decay_times(wvfs::ArrayOfRDWaveforms, config::DSPConfig)
-    dsp_decay_times(wvfs, config.bl_mean, config.pz_fit)
-end
 export dsp_decay_times
+
+dsp_decay_times(wvfs::ArrayOfRDWaveforms, config::DSPConfig) = dsp_decay_times(wvfs, config.bl_window, config.tail_window)
