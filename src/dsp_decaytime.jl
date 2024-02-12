cac85fdf30c84c625c6ab2812e6d02f84dc9b6cc
Florian Henkes <florian.henkes@outlook.com>
2024-02-12 21:12:56 +0100

    Fixed DSP

--- src/dsp_decaytime.jl
+++ src/dsp_decaytime.jl
@@ -10,13 +10,13 @@ Get statistics on the logarhithmic of the tail of the `wvfs` in the interval `ta
 """
 function dsp_decay_times(wvfs::ArrayOfRDWaveforms, bl_window::ClosedInterval{<:Unitful.Time{<:T}}, tail_window::ClosedInterval{<:Unitful.Time{<:T}}) where T <: Real
     # get baseline mean, std and slope
-    bl_stats = signalstats.(wvfs, first(bl_window), last(bl_window))
+    bl_stats = signalstats.(wvfs, leftendpoint(bl_window), rightendpoint(bl_window))
 
     # substract baseline from waveforms
     wvfs_bl = shift_waveform.(wvfs, -bl_stats.mean)
 
     # extract decay times
-    decay_times = tailstats.(wvfs_bl, first(tail_window), last(tail_window))
+    decay_times = tailstats.(wvfs_bl, leftendpoint(tail_window), rightendpoint(tail_window))
     
     # return converted to µs vals
     return uconvert.(u"µs", decay_times.τ)
