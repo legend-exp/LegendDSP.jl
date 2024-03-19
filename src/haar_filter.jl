41867dadd3e0282dd7ed8c1212f5c337d864e5f9
Florian Henkes <florian.henkes@outlook.com>
2024-03-19 20:19:29 +0100

    Fixed issue with Haar filter and initial samples

--- src/haar_filter.jl
+++ src/haar_filter.jl
@@ -1,36 +1,37 @@
 # This file is a part of RadiationDetectorDSP.jl, licensed under the MIT License (MIT).
 
 struct HaarTypeWaveletFilter <: AbstractRadSigFilter{LinearFiltering}
-    sampling_rate::Int
+    down_sampling_rate::Int
     length::Int
+    HaarTypeWaveletFilter(down_sampling_rate::Int) = new(down_sampling_rate, 2)
 end
 
 export HaarTypeWaveletFilter
 
 struct HaarTypeWaveletFilterInstance{T} <: AbstractRadSigFilterInstance{LinearFiltering}
-    sampling_rate::Int
+    down_sampling_rate::Int
     length::Int
     n_input::Int
 end
 
-function fltinstance(flt::HaarTypeWaveletFilter, input::SamplingInfo{T}) where T
-    HaarTypeWaveletFilterInstance{T}(flt.sampling_rate, flt.length, length(input.axis))
+function RadiationDetectorDSP.fltinstance(flt::HaarTypeWaveletFilter, input::SamplingInfo{T}) where T
+    HaarTypeWaveletFilterInstance{T}(flt.down_sampling_rate, flt.length, length(input.axis))
 end
 
-flt_output_smpltype(fi::HaarTypeWaveletFilterInstance) = flt_input_smpltype(fi)
-flt_input_smpltype(::HaarTypeWaveletFilterInstance{T}) where T = T
-flt_output_length(fi::HaarTypeWaveletFilterInstance) = 
-    ceil(Int, fi.n_input/fi.sampling_rate)
-flt_output_time_axis(fi::HaarTypeWaveletFilter, 
-    time::AbstractVector{<:RealQuantity}) = time[1:fi.sampling_rate:end]
+RadiationDetectorDSP.flt_output_smpltype(fi::HaarTypeWaveletFilterInstance) = flt_input_smpltype(fi)
+RadiationDetectorDSP.flt_input_smpltype(::HaarTypeWaveletFilterInstance{T}) where T = T
+RadiationDetectorDSP.flt_output_length(fi::HaarTypeWaveletFilterInstance) = ceil(Int, fi.n_input/fi.down_sampling_rate)
+RadiationDetectorDSP.flt_output_time_axis(fi::HaarTypeWaveletFilterInstance, time::AbstractVector{<:RealQuantity}) = time[1:fi.down_sampling_rate:end]
 
-function rdfilt!(output::AbstractVector{T}, fi::HaarTypeWaveletFilterInstance{T}, input::AbstractVector{T}) where T
+function RadiationDetectorDSP.rdfilt!(output::AbstractVector{T}, fi::HaarTypeWaveletFilterInstance{T}, input::AbstractVector{T}) where T
     invsqrt = inv(sqrt(T(fi.length)))
     @assert flt_output_length(fi) == length(output) "output length not compatible with filter"
-    @inbounds @simd for i in eachindex(output)
-        to = min(fi.n_input, (i-1)*fi.sampling_rate + 1)
-        from = max(1, to + 1 - fi.length)
-        output[i] = sum(view(input, from:to)) * invsqrt
+    Δidx = fi.length - 1
+    output[1] = input[1] / invsqrt
+    output[end] = input[end] / invsqrt
+    for i in 2:length(output)-1
+        j = (i-1)*fi.down_sampling_rate + 1
+        output[i] = sum(view(input, j:j+Δidx)) * invsqrt
     end
     output
 end
