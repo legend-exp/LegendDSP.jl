bef5658ba11459adf163ef1419ed66abd2d4aea6
esleon97 <esleon97@unc.edu>
2024-03-20 15:37:36 +0100

    Renamed to `HaarAveragingFilter`

--- src/haar_filter.jl
+++ src/haar_filter.jl
@@ -1,29 +1,29 @@
 # This file is a part of RadiationDetectorDSP.jl, licensed under the MIT License (MIT).
 
-struct HaarTypeWaveletFilter <: AbstractRadSigFilter{LinearFiltering}
+struct HaarAveragingFilter <: AbstractRadSigFilter{LinearFiltering}
     down_sampling_rate::Int
     length::Int
-    HaarTypeWaveletFilter(down_sampling_rate::Int) = new(down_sampling_rate, 2)
+    HaarAveragingFilter(down_sampling_rate::Int) = new(down_sampling_rate, 2)
 end
 
-export HaarTypeWaveletFilter
+export HaarAveragingFilter
 
-struct HaarTypeWaveletFilterInstance{T} <: AbstractRadSigFilterInstance{LinearFiltering}
+struct HaarAveragingFilterInstance{T} <: AbstractRadSigFilterInstance{LinearFiltering}
     down_sampling_rate::Int
     length::Int
     n_input::Int
 end
 
-function RadiationDetectorDSP.fltinstance(flt::HaarTypeWaveletFilter, input::SamplingInfo{T}) where T
-    HaarTypeWaveletFilterInstance{T}(flt.down_sampling_rate, flt.length, length(input.axis))
+function RadiationDetectorDSP.fltinstance(flt::HaarAveragingFilter, input::SamplingInfo{T}) where T
+    HaarAveragingFilterInstance{T}(flt.down_sampling_rate, flt.length, length(input.axis))
 end
 
-RadiationDetectorDSP.flt_output_smpltype(fi::HaarTypeWaveletFilterInstance) = flt_input_smpltype(fi)
-RadiationDetectorDSP.flt_input_smpltype(::HaarTypeWaveletFilterInstance{T}) where T = T
-RadiationDetectorDSP.flt_output_length(fi::HaarTypeWaveletFilterInstance) = ceil(Int, fi.n_input/fi.down_sampling_rate)
-RadiationDetectorDSP.flt_output_time_axis(fi::HaarTypeWaveletFilterInstance, time::AbstractVector{<:RealQuantity}) = time[1:fi.down_sampling_rate:end]
+RadiationDetectorDSP.flt_output_smpltype(fi::HaarAveragingFilterInstance) = flt_input_smpltype(fi)
+RadiationDetectorDSP.flt_input_smpltype(::HaarAveragingFilterInstance{T}) where T = T
+RadiationDetectorDSP.flt_output_length(fi::HaarAveragingFilterInstance) = ceil(Int, fi.n_input/fi.down_sampling_rate)
+RadiationDetectorDSP.flt_output_time_axis(fi::HaarAveragingFilterInstance, time::AbstractVector{<:RealQuantity}) = time[1:fi.down_sampling_rate:end]
 
-function RadiationDetectorDSP.rdfilt!(output::AbstractVector{T}, fi::HaarTypeWaveletFilterInstance{T}, input::AbstractVector{T}) where T
+function RadiationDetectorDSP.rdfilt!(output::AbstractVector{T}, fi::HaarAveragingFilterInstance{T}, input::AbstractVector{T}) where T
     invsqrt = inv(sqrt(T(fi.length)))
     @assert flt_output_length(fi) == length(output) "output length not compatible with filter"
     @inbounds @simd for i in eachindex(output)
