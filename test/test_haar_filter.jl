9fbb23be70979cd88fdab977205f920aa7093c94
esleon97 <esleon97@unc.edu>
2024-03-20 15:42:24 +0100

    Updated test with new naming.

--- test/test_haar_filter.jl
+++ test/test_haar_filter.jl
@@ -3,7 +3,7 @@ using LegendDSP
 using RadiationDetectorSignals
 using Unitful
 
-@testset "Test HaarTypeWaveletFilter" begin
+@testset "Test HaarAveragingFilter" begin
 
     T = Float64
 
@@ -11,26 +11,26 @@ using Unitful
         
         wvf = RDWaveform(range(0u"μs", step = 32u"μs", length = 256), vcat(ones(T,128), 2 .* ones(T,128)))
         
-        @testset "HaarTypeWaveletFilter(2)" begin
+        @testset "HaarAveragingFilter(2)" begin
 
-            flt = HaarTypeWaveletFilter(2)
+            flt = HaarAveragingFilter(2)
 
-            # Apply HaarTypeWaveletFilter once
+            # Apply HaarAveragingFilter once
             wvf_out = flt(wvf)
             @test wvf_out.time == range(0u"μs", step = 64u"μs", length = 128)
             @test isapprox(wvf_out.signal, vcat(fill(T(sqrt(2)), 64), fill(T(sqrt(8)), 64)), rtol = eps(T))
 
-            # Apply HaarTypeWaveletFilter multiple times
+            # Apply HaarAveragingFilter multiple times
             wvf_out = flt(wvf_out)
             @test wvf_out.time == range(0u"μs", step = 128u"μs", length = 64)
             @test isapprox(wvf_out.signal, vcat(fill(T(2), 32), fill(T(4), 32)), rtol = eps(T))
         end
 
-        @testset "HaarTypeWaveletFilter(4)" begin
+        @testset "HaarAveragingFilter(4)" begin
 
-            flt = HaarTypeWaveletFilter(4)
+            flt = HaarAveragingFilter(4)
 
-            # Apply HaarTypeWaveletFilter once
+            # Apply HaarAveragingFilter once
             wvf_out = flt(wvf)
             @test wvf_out.time == range(0u"μs", step = 128u"μs", length = 64)
             @test isapprox(wvf_out.signal, vcat(fill(T(sqrt(2)), 32), fill(T(sqrt(8)), 32)), rtol = eps(T))
@@ -41,26 +41,26 @@ using Unitful
 
         wvf = RDWaveform(range(0u"μs", step = 32u"μs", length = 256), collect(T, 0:255))
     
-        @testset "HaarTypeWaveletFilter(2)" begin
+        @testset "HaarAveragingFilter(2)" begin
 
-            flt = HaarTypeWaveletFilter(2)
+            flt = HaarAveragingFilter(2)
 
-            # Apply HaarTypeWaveletFilter once
+            # Apply HaarAveragingFilter once
             wvf_out = flt(wvf)
             @test wvf_out.time == range(0u"μs", step = 64u"μs", length = 128)
             @test isapprox(wvf_out.signal, collect(T, (0.5:2:254.5).*sqrt(2)), rtol = eps(T))
 
-            # Apply HaarTypeWaveletFilter multiple times
+            # Apply HaarAveragingFilter multiple times
             wvf_out = flt(wvf_out)
             @test wvf_out.time == range(0u"μs", step = 128u"μs", length = 64)
             @test isapprox(wvf_out.signal, collect(T, 3:8:510))
         end
 
-        @testset "HaarTypeWaveletFilter(4)" begin
+        @testset "HaarAveragingFilter(4)" begin
 
-            flt = HaarTypeWaveletFilter(4)
+            flt = HaarAveragingFilter(4)
 
-            # Apply HaarTypeWaveletFilter once
+            # Apply HaarAveragingFilter once
             wvf_out = flt(wvf)
             @test wvf_out.time == range(0u"μs", step = 128u"μs", length = 64)
             @info diff(wvf_out.signal)
