ebeb53d168caf6ddb6130d34648b9b2cbebcd61c
Florian Henkes <florian.henkes@outlook.com>
2024-05-07 13:40:05 +0200

    Add ML QC evluator for compressed waveforms

--- src/dsp_ml_routines.jl
+++ src/dsp_ml_routines.jl
@@ -19,3 +19,21 @@ function get_qc_classifier(wvfs::ArrayOfRDWaveforms, f_evaluate_qc::Function)
     y_pred, _ = f_evaluate_qc(flatview(VectorOfSimilarArrays(wvfs_flt_haar5.signal)))
     y_pred
 end
+
+
+function get_qc_classifier_compressed(wvfs::ArrayOfRDWaveforms, f_evaluate_qc::Function)
+    # create Haar filter
+    haar_flt = HaarAveragingFilter(2)
+
+    # filter waveforms with Haar filter 2 times
+    wvfs_flt_haar2 = wvfs .|> haar_flt .|> haar_flt
+
+    # normalize with max of absolute extrema values
+    norm_fact = map(x -> max(abs(first(x)), abs(last(x))), extrema.(wvfs_flt_haar2.signal))
+    replace!(norm_fact, 0.0 => one(first(norm_fact)))
+
+    wvfs_flt_haar2 = multiply_waveform.(wvfs_flt_haar2, 1 ./ norm_fact)
+
+    y_pred, _ = f_evaluate_qc(flatview(VectorOfSimilarArrays(wvfs_flt_haar2.signal)))
+    y_pred
+end
