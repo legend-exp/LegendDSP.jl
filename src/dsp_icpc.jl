b2e6315476a8d0a9cdbbecbf8da3a6a7f2a8217e
Florian Henkes <florian.henkes@outlook.com>
2024-05-13 15:57:30 +0200

    Bug Fix --> wvfs_wdw instead of wvfs in lq and qdrift

--- src/dsp_icpc.jl
+++ src/dsp_icpc.jl
@@ -384,10 +384,10 @@ function dsp_icpc_compressed(data::Q, config::DSPConfig, τ::Quantity{T}, pars_f
     drift_time = uconvert.(u"ns", t90 - t0)
   
     # get Q-drift parameter
-    qdrift = get_qdrift(wvfs, t0, qdrift_int_length; pol_power=config.kwargs_pars.int_interpolation_order, sign_est_length=config.kwargs_pars.int_interpolation_length)
+    qdrift = get_qdrift(wvfs_wdw, t0, qdrift_int_length; pol_power=config.kwargs_pars.int_interpolation_order, sign_est_length=config.kwargs_pars.int_interpolation_length)
 
     # get LQ parameter
-    lq  = get_qdrift(wvfs, t80, lq_int_length; pol_power=config.kwargs_pars.int_interpolation_order, sign_est_length=config.kwargs_pars.int_interpolation_length)
+    lq  = get_qdrift(wvfs_wdw, t80, lq_int_length; pol_power=config.kwargs_pars.int_interpolation_order, sign_est_length=config.kwargs_pars.int_interpolation_length)
 
     # robust energy reconstruction with long, middle and short rise and flat-top times
     uflt_10410 = TrapezoidalChargeFilter(10u"µs", 4u"µs")
